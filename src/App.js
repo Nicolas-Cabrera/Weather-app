16b53506c7555212d081b67169992cd47260565e
Nicolas Cabrera <nicolascc1@hotmail.com>
2019-12-23 11:46:49 +0000

    Set variables to be dynamic and added weather images

--- src/App.js
+++ src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Output from './Output';
 import Input from './Input';
+const APIkey = 'f474c5ad41135c5df510a79853a2b255';
 
-function App() {
+export default function App() {
+
+  // const [getWeather, setGetWeather] = useState(getWeatherAPI);
+  const [city, setCity] = useState('');
+  const [country, setCountry] = useState('');
+  const [icon, setIcon] = useState('');
+  const [celsius, setCelsius] = useState('');
+  const [tempMax, setTempMax] = useState('');
+  const [tempMin, setTempMin] = useState('');
+  const [description, setDescription] = useState('');
+
+  //Weather Icons = Thunderstorm, Drizzle, Rain, Snow, Atmosphere, Clear, Cloudy
+
+  function calcCelsius(temp) {
+    let celsius = Math.round(temp - 273.15);
+    return celsius;
+  }
+
+  const getWeatherAPI = async () => {
+      try {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${APIkey}`)
+        const res = await api_call.json();
+        setCity(res.name);
+        setCountry(res.sys.country);
+        setCelsius(calcCelsius(res.main.temp));
+        setTempMin(calcCelsius(res.main.temp_min));
+        setTempMax(calcCelsius(res.main.temp_max));
+        setDescription(res.weather[0].description)
+        console.log(res);
+      } catch(e) {
+        console.error("problem", e)
+      }
+  }
+  getWeatherAPI();
+
   return (
     <div className="App">
-        <h1>Weather App</h1>
-        <Input />
+      <h1>Weather App</h1>
+      <Input />
+      <Output city={city} country={country} tempCelcius={celsius} tempMax={tempMax} tempMin={tempMin} description={description}/>
     </div>
   );
+
 }
 
-export default App;
---
+++ src/Output.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function Output(props) {
+    
+    return (
+        <div>
+            <div>
+                <h1>{props.city}, {props.country}</h1>
+                <h2>{props.tempCelcius}&deg;</h2>
+                {
+                    minmaxTemp(props.tempMin, props.tempMax)
+                }
+                <h1>{props.description}</h1>
+            </div>
+        </div>
+    );
+
+    function minmaxTemp(min, max) {
+        return (
+            <div>
+                <h1>Min Temp: {min}&deg;</h1>
+                <h1>Max Temp: {max}&deg;</h1>
+            </div>
+        );
+    }
+}
